f9af5e96392b5833d3a1da895eb4499beb4afb4c
wisedeve <18612124664@163.com>
2017-09-05 17:02:24 +0800

    paint基础练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,15 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -42,35 +49,59 @@ public class Practice12PathEffectView extends View {
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        //把所有拐角变成圆角。
+        paint.setPathEffect(new CornerPathEffect(20));
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+        // 把线条进行随机的偏离，让轮廓变得乱七八糟。乱七八糟的方式和程度由参数决定。
+        paint.setPathEffect(new DiscretePathEffect(20,5));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
         // 第三处：DashPathEffect
+        //使用虚线来绘制线条。
+        paint.setPathEffect(new DashPathEffect(new float[]{10,5},0));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+        //这个方法比 DashPathEffect 多一个前缀 Path ，所以顾名思义，它是使用一个 Path 来绘制「虚线」
+        Path dashPath = new Path();
+        dashPath.addCircle(0,0,10, Path.Direction.CW);
+        paint.setPathEffect(new PathDashPathEffect(dashPath,40,0,PathDashPathEffect.Style.TRANSLATE));
+        /*
+        canPathDashPathEffect.Style ，是一个 enum ，具体有三个值：
+        TRANSLATE：位移
+        ROTATE：旋转
+        MORPH：变体
+        */
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+        //组合效果类的 PathEffect。
+        // 就是分别按照两种 PathEffect 分别对目标进行绘制。
+        PathEffect dashEffect = new DashPathEffect(new float[]{20, 10}, 0);
+        PathEffect discreteEffect = new DiscretePathEffect(20, 5);
+        paint.setPathEffect(new SumPathEffect(dashEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+        //这也是一个组合效果类的 PathEffect 。
+        // 不过它是先对目标 Path 使用一个 PathEffect，然后再对这个改变后的 Path 使用另一个 PathEffect。
+        paint.setPathEffect(new ComposePathEffect(dashEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
     }
